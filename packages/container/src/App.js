2783fed00354fadf3b1b5b1008d064e305ef75f1
Samuel <samuel.imafidon-ext@gmail.com>
2022-10-12 23:25:33 +0100

    chore: achange text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there, this is the container</h1>
+      <h1>Hi guys</h1>
       <hr />
       <MarketingApp />
     </div>
